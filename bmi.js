0ab9a8b9d62bbbfa7fadbb8ac47e3619ac501461
Sattarkha <156591821+Sattarkha@users.noreply.github.com>
2024-03-22 14:27:03 +0500

    mybmi calculator

---
+++ bmi.js
@@ -0,0 +1,5 @@
+// let weightinkg: number = 70;
+// let heightinmeters: number = 3
+// let BMI: number = weightinkg/(heightinmeters*heightinmeters);
+// console.log(`my bmi is ${BMI}`);
+console.log("A B C D E F GHIJKLMNOPQRSTUVWXYZ");
---
+++ bmi.ts
@@ -0,0 +1,5 @@
+// let weightinkg: number = 70;
+// let heightinmeters: number = 3
+// let BMI: number = weightinkg/(heightinmeters*heightinmeters);
+// console.log(`my bmi is ${BMI}`);
+
